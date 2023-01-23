a1a101078d2a149f82cca0dcba0a4138e469d9ec
SirLucasLima <doislcompany@gmail.com>
2023-01-23 08:40:44 -0300

    update

--- app/page.js
+++ app/page.js
@@ -1,17 +1,10 @@
 import { Footer, Navbar } from '../components';
-import { About, Explore, Feedback, GetStarted, Hero, Insights, WhatsNew, World } from '../sections';
+import { Hero } from '../sections';
 
 const Page = () => (
   <div>
     <Navbar />
     <Hero />
-    <About />
-    <Explore />
-    <GetStarted />
-    <WhatsNew />
-    <World />
-    <Insights />
-    <Feedback />
     <Footer />
   </div>
 );
