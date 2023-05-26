7b4e94dee429a29bab1e4a12b165457067707704
Developer Lucas Lima <developerlucaslima@gmail.com>
2023-05-26 11:26:41 -0300

    create user

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -1,7 +1,23 @@
 import { FastifyInstance } from 'fastify'
+import { z } from 'zod'
+import { prisma } from '../lib/prisma'
 
 export async function usersRoute(app: FastifyInstance) {
-  app.get('/users', (request, reply) => {
-    reply.send({ message: 'Hello World!' })
+  app.post('/users', async (request) => {
+    const registerBodySchema = z.object({
+      username: z.string().min(3),
+      password: z.string().min(6),
+    })
+
+    const { username, password } = registerBodySchema.parse(request.body)
+
+    const user = await prisma.user.create({
+      data: {
+        username,
+        password,
+      },
+    })
+
+    return user
   })
 }
