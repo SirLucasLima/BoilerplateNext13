744e0bcb5f514b3b4d8e675a741c0141d06bea64
Developer Lucas Lima <developerlucaslima@gmail.com>
2023-05-27 09:54:06 -0300

    get products

--- src/routes/products.ts
+++ src/routes/products.ts
@@ -1,18 +1,18 @@
 import { FastifyInstance } from 'fastify'
+import { AppError } from '../utils/AppError'
 import { prisma } from '../lib/prisma'
 
 export async function productsRoutes(app: FastifyInstance) {
-  app.get('/products', async () => {
-    const products = await prisma.products.findMany({
-      orderBy: {
-        createdAt: 'asc',
-      },
-    })
+  app.addHook('preHandler', async (request) => {
+    const authheader = await request.headers.authorization
 
-    return products.map((product: { id: any }) => {
-      return {
-        id: product.id,
-      }
-    })
+    if (!authheader) {
+      throw new AppError('Invalid JWT token', 401)
+    }
+  })
+
+  app.get('/products', async (request, reply) => {
+    const products = await prisma.product.findMany()
+    reply.send(products)
   })
 }
