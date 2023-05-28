5c5293dbb4ed86b727f077230ff8eff7fc6a9e85
JoviTech <developerlucaslima@gmail.com>
2023-05-27 21:33:59 -0300

    products crud

--- src/routes/products.ts
+++ src/routes/products.ts
@@ -1,59 +1,108 @@
 import { FastifyInstance } from 'fastify'
-import { AppError } from '../utils/AppError'
-import { prisma } from '../lib/prisma'
-import { verify } from 'jsonwebtoken'
-import { authConfig } from '../configs/auth'
 import { z } from 'zod'
+import { prisma } from '../lib/prisma'
+// import { AppError } from '../utils/AppError'
 
-export async function productsRoutes(app: FastifyInstance) {
-  app.addHook('preHandler', async (request) => {
-    const authHeader = request.headers.authorization
-
-    if (!authHeader) {
-      throw new AppError('Missing JWT token', 401)
-    }
-
-    try {
-      const [, token] = authHeader.split(' ')
-      const { secret } = authConfig.jwt
-
-      const decoded = verify(token, secret) as { sub: string }
-      request.id = decoded.sub // Armazena o ID do usuário na propriedade personalizada
-    } catch (error) {
-      throw new AppError('Invalid JWT token', 401)
-    }
-  })
-
+export async function productsRoute(app: FastifyInstance) {
+  // List all products
   app.get('/products', async (request, reply) => {
     const products = await prisma.product.findMany()
-    reply.send(products)
+    return reply.send(products)
   })
 
+  // Search by name
   app.get('/products/search', async (request, reply) => {
-    const productSearchQuerySchema = z.object({
-      name: z.string().optional(),
-      description: z.string().optional(),
-      price: z.number().optional(),
+    const createProductSchema = z.object({
+      name: z.string().min(3),
     })
 
-    const { name, description, price } = productSearchQuerySchema.parse(
-      request.query,
-    )
+    const { name } = createProductSchema.parse(request.body)
 
-    const filteredProducts = await prisma.product.findMany({
+    const products = await prisma.product.findMany({
       where: {
         name: {
-          contains: name || undefined,
-        },
-        description: {
-          contains: description || undefined,
-        },
-        price: {
-          equals: price || undefined,
+          contains: name,
         },
       },
     })
 
-    reply.send(filteredProducts)
+    return reply.send(products)
+  })
+
+  // Create a new product
+  app.post('/products', async (request, reply) => {
+    const createProductSchema = z.object({
+      userId: z.string(),
+      name: z.string().min(3),
+      description: z.string().min(15),
+      price: z.number().positive(),
+      image: z.string().url(),
+    })
+
+    const { userId, name, description, price, image } =
+      createProductSchema.parse(request.body)
+
+    const product = await prisma.product.create({
+      data: {
+        userId,
+        name,
+        description,
+        price,
+        image,
+      },
+    })
+
+    return reply.send(product)
+  })
+
+  // Update an existing product
+  app.put('/products/:id', async (request, reply) => {
+    const idSchema = z.object({
+      id: z.string(),
+    })
+
+    const { id } = idSchema.parse(request.params)
+
+    const updateProductSchema = z.object({
+      name: z.string().min(3).optional(),
+      description: z.string().min(5).optional(),
+      price: z.number().positive().optional(),
+      image: z.string().url().optional(),
+    })
+
+    const { name, description, price, image } = updateProductSchema.parse(
+      request.body,
+    )
+
+    const updatedProduct = await prisma.product.update({
+      where: {
+        id: parseInt(id),
+      },
+      data: {
+        name,
+        description,
+        price,
+        image,
+      },
+    })
+
+    return reply.send(updatedProduct)
+  })
+
+  // Delete product
+  app.delete('/products/:id', async (request, reply) => {
+    const idSchema = z.object({
+      id: z.string(),
+    })
+
+    const { id } = idSchema.parse(request.params)
+
+    await prisma.product.delete({
+      where: {
+        id: parseInt(id),
+      },
+    })
+
+    return reply.send({ message: 'Product deleted successfully' })
   })
 }
